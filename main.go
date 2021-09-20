3220e6f5a67a965259c217c6f36ddd5fbed216f4
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2021-09-20 19:37:16 -0300

    first server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("hello microservice")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("hello world")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("Ooops"))
+			// all of this can be replaced by
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Data %s\n", d)
+
+		fmt.Fprintf(rw, "Hello again %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("goodbye world")
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
